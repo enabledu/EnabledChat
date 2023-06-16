c768fa1848b2e000b9c0257852df899e374bc37f
Mahmoud Hussein <mahmoudh3257@gmail.com>
2023-06-16 18:12:47 +0300

    Another damn typo

--- app/utils.py
+++ app/utils.py
@@ -3,13 +3,13 @@ Utility functions for the app
 """
 
 import torch
-from transformers import AutoTokenizer, AutoModelForCasualLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 def load_model_and_tokenizer(model_name):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCasualLM.from_pretrained(
+    model = AutoModelForCausalLM.from_pretrained(
         model_name,
         trust_remote_code=True,
         torch_dtype=torch.float16,
